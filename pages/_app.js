e179035ce0e2ac7439e24e13dc3532f73cf43708
epletnev <e.a.pletnev@gmail.com>
2021-07-16 10:27:21 +0100

    Changed files for deployment to vercel
    Co-authored-by: Nafisa Axmed <nafisa@Nafisas-MacBook-Air.local>

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,8 @@
-import { Auth } from '@supabase/ui';
-import { supabase } from '../lib/initSupabase';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<Auth.UserContextProvider supabaseClient={supabase}>
-			<Component {...pageProps} />
-		</Auth.UserContextProvider>
-	);
+    return <Component {...pageProps }
+    />;
 }
 
 export default MyApp;

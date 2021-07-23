c1ca666e3b933cd5e0ae03d92e33a6ef6ee33531
epletnev <e.a.pletnev@gmail.com>
2021-07-23 17:45:00 +0100

    changed id file routes
    Co-authored-by: Nafisa Axmed <axmednafisa@gmail.com>

--- pages/[id].js
+++ pages/[id].js
@@ -8,20 +8,7 @@ import logo from '../public/images/logo.svg';
 import Image from 'next/image';
 import Footer from '../components/Footer/Footer';
 
-export async function getStaticPaths() {
-	const { data } = await supabase.from('impact_areas').select('id');
-	const paths = data.map((area) => ({
-		params: {
-			id: JSON.stringify(area.id),
-		},
-	}));
-	return {
-		paths,
-		fallback: true,
-	};
-}
-
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
 	const { id } = params;
 	const { data } = await supabase
 		.from('questions')
@@ -30,12 +17,13 @@ export async function getStaticProps({ params }) {
 
 	return {
 		props: {
+			id: id,
 			question: data,
 		},
 	};
 }
 
-export default function Post({ question }) {
+export default function Post({ question, id }) {
 	const { user } = Auth.useUser();
 	const router = useRouter();
 	if (router.isFallback) {
@@ -64,37 +52,39 @@ export default function Post({ question }) {
 					<main className={styles.main}>
 						<div>
 							<h1 className={styles.title}>{question[0].impact_area_name}</h1>
-							{question.map((question) => (
-								<div className={styles.card} key={question.id.toString()}>
-									<p>{question.name}</p>
-									<div className={styles.input}>
-										<input
-											type='radio'
-											id='yes'
-											name={question.id.toString()}
-											value='yes'
-										/>
-										<label htmlFor='yes'>Yes</label>
-										<input
-											type='radio'
-											id='no'
-											name={question.id.toString()}
-											value='no'
-										/>
-										<label htmlFor='no'>No</label>
+							<form action='/' method='POST' onSubmit={handleSubmit}>
+								{question.map((question) => (
+									<div className={styles.card} key={question.id.toString()}>
+										<p>{question.name}</p>
+										<div className={styles.input}>
+											<input
+												type='radio'
+												id='yes'
+												name={question.id.toString()}
+												value='yes'
+											/>
+											<label htmlFor='yes'>Yes</label>
+											<input
+												type='radio'
+												id='no'
+												name={question.id.toString()}
+												value='no'
+											/>
+											<label htmlFor='no'>No</label>
+										</div>
 									</div>
-								</div>
-							))}
+								))}
+							</form>
 						</div>
-						<Link href='/'>
-							<a>
-								<input
-									className={styles.button}
-									type='submit'
-									value='Submit'
-								></input>
-							</a>
-						</Link>
+						{/* <Link href='/'> */}
+						{/* <a> */}
+						<input
+							className={styles.button}
+							type='submit'
+							value='Submit'
+						></input>
+						{/* </a> */}
+						{/* </Link> */}
 					</main>
 					<Footer />
 				</div>

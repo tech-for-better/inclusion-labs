8c60e7d487fdb2de9d04c5d7199aeca7a0945971
Nafisa Axmed <axmednafisa@gmail.com>
2021-07-28 11:15:58 +0100

    insert survey answers into db table
    Co-authored-by: Evgeny Pletnev <e.a.pletnev@gmail.com>

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -3,26 +3,16 @@ import { supabase } from '../../api';
 const addAnswers = async (req, res) => {
 	const { user } = await supabase.auth.api.getUserByCookie(req);
 	if (req.method === 'POST') {
-		// for (const [key, value] of Object.entries(req.body)) {
-		// 	supabase.from('answers').insert([
-		// 		{
-		// 			question_id: key,
-		// 			answer: value,
-		// 			user_id: user.id,
-		// 		},
-		// 	]);
-
-		supabase
-			.from('answers')
-			.insert([
+		for (const [key, value] of Object.entries(req.body)) {
+			console.log(key, value);
+			await supabase.from('answers').insert([
 				{
-					question_id: 1,
-					answer: 'yes',
+					question_id: key,
+					answer: value,
 					user_id: user.id,
 				},
-			])
-			.single();
-
+			]);
+		}
 		res.redirect('/');
 	} else {
 		res.status(404).send('Not found');

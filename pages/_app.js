8cdafeeee6abd02b0c9c34457ec7c17447a48149
epletnev <e.a.pletnev@gmail.com>
2021-07-28 17:41:40 +0100

    Removed commented out code
    Co-authored-by: Nafisa Axmed <axmednafisa@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,12 @@
 import { Auth } from '@supabase/ui';
 import { supabase } from '../api';
-// import HeadFunction from '../components/Head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	return (
-		// <>
-		// 	<HeadFunction />
 		<Auth.UserContextProvider supabaseClient={supabase}>
 			<Component {...pageProps} />
 		</Auth.UserContextProvider>
-		// </>
 	);
 }
 
